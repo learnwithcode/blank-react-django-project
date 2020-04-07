3c1dffb340b6af274e131fbe15ae64a8060bd23b
SUprem Nandal <suprem.nandal@gmail.com>
2020-04-07 14:02:32 +0530

    SSL Settings

--- mysite/settings/pro.py
+++ mysite/settings/pro.py
@@ -8,3 +8,14 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+# SSL =====================================================================================================================
+CORS_REPLACE_HTTPS_REFERER      = True
+HOST_SCHEME                     = "https://"
+SECURE_PROXY_SSL_HEADER         = ('HTTP_X_FORWARDED_PROTO', 'https')
+SECURE_SSL_REDIRECT             = True
+SESSION_COOKIE_SECURE           = True
+CSRF_COOKIE_SECURE              = True
+SECURE_HSTS_INCLUDE_SUBDOMAINS  = True
+SECURE_HSTS_SECONDS             = 1000000
+SECURE_FRAME_DENY               = True
